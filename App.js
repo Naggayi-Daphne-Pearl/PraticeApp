e374144b7ea7fa8981b5825fda7e8ccacb814a40
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-05-11 13:34:31 +0300

    expo eject

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
+  const [authResult, setAuthResult] = useState(null);
+
+  const authenticateWithBiometrics = async () => {
+    try {
+      const result = await LocalAuthentication.authenticateAsync();
+      setAuthResult(result);
+    } catch (error) {
+      console.error('Biometric authentication error:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Biometric Authentication</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={authenticateWithBiometrics}
+      >
+        <Text style={styles.buttonText}>Authenticate with Biometrics</Text>
+      </TouchableOpacity>
+      {authResult && (
+        <>
+          {authResult.success ? (
+            <Text style={styles.successText}>Authentication successful!</Text>
+          ) : (
+            <Text style={styles.errorText}>Authentication failed: {authResult.error}</Text>
+          )}
+        </>
+      )}
     </View>
   );
 }
@@ -16,5 +42,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 12,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  successText: {
+    color: 'green',
+    marginTop: 12,
+    marginBottom: 12,
+  },
+  errorText: {
+    color: 'red',
+    marginTop: 12,
+    marginBottom: 12,
   },
 });
